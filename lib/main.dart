31d9dcb73fa68d72b7457ba4c239d29882dbf2a8
Elango <nithiskrishnan43@gmail.com>
2023-12-06 12:27:28 +0530

    check

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/Document/FileScreen.dart';
 import 'package:chatapp/Premission/CameraScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: PhoneScreen());
+        home:FiledowloadScreen());
   }
 }
