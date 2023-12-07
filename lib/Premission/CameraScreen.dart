ae405a5fb25861486a9009f3819bf3df3402eb57
Elango <nithiskrishnan43@gmail.com>
2023-12-07 16:45:46 +0530

    update

--- lib/Premission/CameraScreen.dart
+++ lib/Premission/CameraScreen.dart
@@ -19,7 +19,7 @@ class _CameraScreenState extends State<CameraScreen> {
       Permission.phone,
       Permission.contacts,
       Permission.notification,
-      Permission.storage
+
     ].request();
     print(status);
   }
